1baa63e15d117da87c45bff318e0ae3a037703b3
PoojaRavi8902 <ravirajubelithala@gmail.com>
2026-03-11 20:50:50 +0200

    Add files via upload

---
+++ network_answers.js
@@ -0,0 +1,110 @@
+const mcq=[
+
+{q:"Which device forwards data packets between different networks?",a:"Switch",b:"Router",c:"Hub",correct:"b"},
+
+{q:"What does LAN stand for?",a:"Local Area Network",b:"Large Access Network",c:"Local Access Node",correct:"a"},
+
+{q:"Which type of network covers a large geographical area?",a:"LAN",b:"WAN",c:"PAN",correct:"b"},
+
+{q:"Which protocol automatically assigns IP addresses to devices?",a:"FTP",b:"DHCP",c:"HTTP",correct:"b"},
+
+{q:"Which device connects multiple devices within a LAN?",a:"Switch",b:"Router",c:"Modem",correct:"a"},
+
+{q:"Which network topology uses a central device?",a:"Star",b:"Ring",c:"Bus",correct:"a"},
+
+{q:"Which system converts domain names into IP addresses?",a:"DNS",b:"DHCP",c:"FTP",correct:"a"},
+
+{q:"Which device sends data to all connected devices?",a:"Hub",b:"Switch",c:"Router",correct:"a"},
+
+{q:"Which network type is used within a school building?",a:"WAN",b:"LAN",c:"MAN",correct:"b"},
+
+{q:"Which technology allows wireless networking?",a:"WiFi",b:"USB",c:"HDMI",correct:"a"}
+
+]
+const fill=[
+
+"Device that directs traffic between networks",
+"Small network within a building",
+"Large network covering many cities",
+"Unique identifier assigned to each device",
+"System translating domain names into IP addresses",
+"Device connecting computers inside LAN",
+"Computer that provides resources in a network",
+"Computer requesting resources",
+"Arrangement of devices in a network",
+"Speed at which data is transferred"
+
+]
+
+const fillAnswers=[
+
+"cm91dGVy",
+"bGFu",
+"d2Fu",
+"aXAgYWRkcmVzcw==",
+"ZG5z",
+"c3dpdGNo",
+"c2VydmVy",
+"Y2xpZW50",
+"dG9wb2xvZ3k",
+"YmFuZHdpZHRo"
+
+]
+const match=[
+
+"Router → __________",
+"Switch → __________",
+"Hub → __________",
+"DNS → __________",
+"DHCP → __________",
+"LAN → __________",
+"WAN → __________",
+"Server → __________",
+"Client → __________",
+"Topology → __________"
+
+]
+
+const matchAnswers=[
+
+"cm91dGVzIHBhY2tldHM=",
+"Y29ubmVjdHMgZGV2aWNlcw==",
+"YnJvYWRjYXN0cw==",
+"ZG9tYWluIG5hbWUgc3lzdGVt",
+"aXAgYWRkcmVzcyBhc3NpZ25tZW50",
+"bG9jYWwgbmV0d29yaw==",
+"d2lkZSBuZXR3b3Jr",
+"cHJvdmlkZXMgcmVzb3VyY2Vz",
+"cmVxdWVzdHMgcmVzb3VyY2Vz",
+"bmV0d29yayBsYXlvdXQ="
+
+]
+const shortq=[
+
+"Define a router",
+"What is a switch in networking?",
+"What is a LAN?",
+"What is a WAN?",
+"What is the purpose of DNS?",
+"What is an IP address?",
+"What is a server?",
+"What is a client computer?",
+"What is bandwidth?",
+"What is network topology?"
+
+]
+
+const shortAnswers=[
+
+"cm91dGVy",
+"c3dpdGNo",
+"bGFu",
+"d2Fu",
+"ZG5z",
+"aXAgYWRkcmVzcw==",
+"c2VydmVy",
+"Y2xpZW50",
+"YmFuZHdpZHRo",
+"dG9wb2xvZ3k"
+
+]
